8ed2ecf19392a067cc884f81b19987ee756cc92d
parasaiteve <101370780+parasaiteve@users.noreply.github.com>
2023-04-10 20:08:57 -0300

    update

--- ptr_null.cpp
+++ ptr_null.cpp
@@ -1,18 +1,30 @@
+//incluir biblioteca
 #include <iostream>
+//biblioteca chamada para usar
 using namespace std;
 
+//inicio do programa
 int main()
 {
+    //declaracao da variavel
     int *ptr = new int;
+    //ponteiro pVar
     *ptr = 7;
+    //atribuir o valor a ele
     
+    //envia o valor do ponteiro
     cout<< *ptr<<endl;
+    //imprime o endereco
     cout<< ptr<<endl;
     
+    //deleta o ponteiro
     delete ptr;
+    //limpa a memoria suja
     ptr = nullptr;
     
+    //envia o valor do ponteiro
     cout<< *ptr<<endl;
+    //imprime o endereco
     cout<< ptr<<endl;
     
 }
